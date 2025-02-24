11940417ef1e3f34b23e29b95c095aee8e5062a4
YounessFTN <youness.fatine1@gmail.com>
2025-02-24 15:52:05 +0100

    change eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Exemple d'ignorer certaines règles
+      "@typescript-eslint/no-unused-vars": "off", // Désactive la règle no-unused-vars
+      "react-hooks/exhaustive-deps": "warn", // Modifie le niveau de cette règle
+    },
+  },
 ];
 
 export default eslintConfig;
